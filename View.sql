420be66e8e35bdb22882dd975b3b1aaa16564a82
Shadow <tekken235@gmail.com>
2017-07-03 14:52:04 +0200

    View Update

--- View.sql
+++ View.sql
@@ -1,5 +1,18 @@
 -- Create a View
-CREATE VIEW Employee
+CREATE VIEW EmployeeView
 (id,[givenname],[familyname])
+AS
 SELECT EmployeeID, FirstName, LastName
-FROM Employee
+FROM Employee;
+GO
+
+-- Encrypt view (obfuscating)
+CREATE VIEW EmployeeView WITH ENCRYPTION
+AS
+SELECT EmployeeID, FirstName, LastName
+FROM Employee;
+GO
+
+-- Drop view
+DROP VIEW Employee;
+GO
