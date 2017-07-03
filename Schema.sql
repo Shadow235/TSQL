d81f57def02763b02cd5858bc923a31994db7aa7
Shadow <tekken235@gmail.com>
2017-07-03 16:10:58 +0200

    delete

--- Schema.sql
+++
@@ -1,9 +0,0 @@
--- Create a schema
-CREATE SCHEMA Reporting AUTHORIZATION dbo; -- [dbo] - user (database owner)
-GO
-
--- Add Table to schema
-CREATE TABLE Reporting.NewTable (
-    ID BIGINT IDENTITY(1,1) PRIMARY KEY
-);
-GO
